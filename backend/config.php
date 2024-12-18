81b4647477b85688ce2ded7b921e8be74fe53dd2
Hkz0 <haikhalrapi@gmail.com>
2024-12-18 16:00:52 +0800

    db connection

--- backend/config.php
+++ backend/config.php
@@ -1,1 +1,8 @@
-NIGGA
+<?php
+
+$db_host = "192.168.0.250";
+$db_user = "website";
+$db_pass = "website1";
+$db_name = "KAMPUNG_KITCHEN";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
