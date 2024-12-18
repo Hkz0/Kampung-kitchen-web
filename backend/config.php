870b85ec48302e998e045de28722e71965e2b42f
Hkz0 <haikhalrapi@gmail.com>
2024-12-18 18:23:13 +0800

    a

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,12 @@
 <?php
-
+// database connection
 $db_host = "192.168.0.250";
 $db_user = "website";
 $db_pass = "website1";
 $db_name = "KAMPUNG_KITCHEN";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$conn) {// check connection
+    die("Connection failed: " . mysqli_connect_error());
+}
