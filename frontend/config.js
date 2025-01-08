da7441357d8242e76df7a42489aee2b4109e1011
Hkz0 <haikhalrapi@gmail.com>
2025-01-08 19:27:33 +0800

    real config

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://http://end-bangladesh.gl.at.ply.gg:56189/kampungKitchen/api/';
+export const BASE_URL = 'http://end-bangladesh.gl.at.ply.gg:56189/kampungKitchen/api/';
 
 // export const BASE_URL = 'http://localhost/kampung-kitchen-web/api/';
