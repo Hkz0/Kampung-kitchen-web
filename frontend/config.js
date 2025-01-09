624d1d4a06a6fada11035d39cd9520518d4fa7a0
Hkz0 <haikhalrapi@gmail.com>
2025-01-09 12:32:00 +0800

    fix

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost/kampung-kitchen-web/api/';
+export const BASE_URL = 'http://end-bangladesh.gl.at.ply.gg:56189/kampungKitchen/api';
 
-//http://end-bangladesh.gl.at.ply.gg:56189/kampungKitchen/api
+//
